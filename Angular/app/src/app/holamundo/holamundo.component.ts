c6ae8a921bb65ea7647d97599d420bb613441650
Danaxar <daniel.catalan@usach.cl>
2024-04-17 19:25:45 -0400

    Se rehace el proyecto con --no-standalone

---
+++ Angular/app/src/app/holamundo/holamundo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-holamundo',
+  templateUrl: './holamundo.component.html',
+  styleUrl: './holamundo.component.css'
+})
+export class HolamundoComponent {
+  variable = 'Hola MundoXD';
+}
